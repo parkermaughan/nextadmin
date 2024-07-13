959c2e62d08a75f69936d0253443a825309af1bf
Parker Maughan <parkermaughan@gmail.com>
2024-07-12 23:24:52 -0400

    Navbar section

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -10,6 +10,7 @@ import {
     MdPeople,
     MdOutlineSettings,
     MdHelpCenter,
+    MdLogout,
 } from 'react-icons/md';
 import Image from 'next/image';
 
@@ -102,6 +103,7 @@ const Sidebar = () => {
                         </li>
                     ))}
                 </ul>
+                <button className={styles.logout}><MdLogout />Logout</button>
             </div>
         </div>
     );
